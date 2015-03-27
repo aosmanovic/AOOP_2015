a098879eed99154ee094de22bdd194b5b2b4a85a
David Seebacher <dseebacher@gmail.com>
2015-03-27 19:58:52 +0100

    Add the project layout

---
+++ ass1/src/main/java/at/ac/tuwien/foop/App.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.foop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	private static Logger log = LoggerFactory.getLogger(App.class);
+	public static void main(String[] args) {
+		log.info("system start");
+	}
+}
