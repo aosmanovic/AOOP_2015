5fddfe16e36d54d61dea57f6a3cff9c308b0a1e4
David Seebacher <dseebacher@gmail.com>
2015-03-30 10:29:27 +0200

    Add client connection retry

--- ass1/src/main/java/at/ac/tuwien/foop/client/NettyClient.java
+++ ass1/src/main/java/at/ac/tuwien/foop/client/NettyClient.java
@@ -1,6 +1,8 @@
 package at.ac.tuwien.foop.client;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
@@ -12,6 +14,8 @@ import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,17 +25,23 @@ import at.ac.tuwien.foop.message.MessageEncoder;
 public class NettyClient implements Runnable {
 	private static Logger log = LoggerFactory.getLogger(NettyClient.class);
 
+	private final int MAX_RETRY = 10;
+	private final int RETRY_TIME_MS = 1000;
+	private final CountDownLatch latch = new CountDownLatch(1);
+
 	private String host = "localhost";
 	private int port = 20150;
+	private int retry = 0;
+	EventLoopGroup workerGroup = new NioEventLoopGroup();
+
 	private Game game;
-	
+
 	public NettyClient(Game game) {
 		this.game = game;
 	}
 
 	public void run() {
 		log.info("start client");
-		EventLoopGroup workerGroup = new NioEventLoopGroup();
 		Bootstrap bootstrap = new Bootstrap();
 		bootstrap.group(workerGroup).channel(NioSocketChannel.class)
 				.option(ChannelOption.SO_KEEPALIVE, true)
@@ -39,8 +49,7 @@ public class NettyClient implements Runnable {
 					@Override
 					protected void initChannel(SocketChannel ch)
 							throws Exception {
-						ch.pipeline().addLast(
-								new LineBasedFrameDecoder(256));
+						ch.pipeline().addLast(new LineBasedFrameDecoder(256));
 						ch.pipeline().addLast(
 								new StringDecoder(CharsetUtil.UTF_8));
 						ch.pipeline().addLast(
@@ -49,12 +58,45 @@ public class NettyClient implements Runnable {
 						ch.pipeline().addLast(new ClientHandler(game));
 					}
 				});
+		connect(bootstrap);
 		try {
-			bootstrap.connect(host, port).sync().channel().closeFuture().sync();
+			latch.await();
 		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-			workerGroup.shutdownGracefully();
 		}
+		workerGroup.shutdownGracefully();
+		log.info("_client done..");
+	}
+
+	private ChannelFuture connect(Bootstrap bootstrap) {
+		log.debug("connect to {}:{}", host, port);
+		ChannelFuture future = bootstrap.connect(host, port);
+		future.addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(ChannelFuture future)
+					throws Exception {
+				log.info("connection operation completed...");
+				if (!future.isSuccess()) {
+					if (retry >= MAX_RETRY) {
+						log.warn("could not connect to server, give up :(!");
+						latch.countDown();
+						System.out.println(future.channel());
+						future.channel().close();
+						log.info("closed!?");
+						return;
+					}
+					retry++;
+					log.warn("could not connect, will retry ({}/{})!", retry,
+							MAX_RETRY);
+					 Thread.sleep(RETRY_TIME_MS);
+					 connect(bootstrap);
+				} else {
+					future.channel().closeFuture().sync();
+					latch.countDown();
+				}
+			}
+		});
+		return future;
 	}
 }
