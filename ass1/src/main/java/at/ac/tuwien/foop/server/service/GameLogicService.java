2d05dcc6bf62e9f1f65a60373717b95313fbe896
Alma Causevic <alma.causevic@hotmail.com>
2015-05-15 17:08:30 +0200

    coordinates

--- ass1/src/main/java/at/ac/tuwien/foop/server/service/GameLogicService.java
+++ ass1/src/main/java/at/ac/tuwien/foop/server/service/GameLogicService.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.foop.server.service;
 
 import java.util.Scanner;
 
+import at.ac.tuwien.foop.client.domain.Coordinates;
 import at.ac.tuwien.foop.domain.Player;
 import at.ac.tuwien.foop.server.domain.BoardString;
 import at.ac.tuwien.foop.server.domain.Game;
@@ -9,18 +10,18 @@ import at.ac.tuwien.foop.server.domain.Game;
 public class GameLogicService {
 
 	private static String BOARD_PATH = "Map.txt";
-	
+
 	public BoardString getBoard(Game game) {
 		if (game.board() != null) {
 			return game.board();
 		}
-		
+
 		// TODO: load random board
 		BoardString bs = loadBoard(BOARD_PATH);
 		game.setBoard(bs);
 		return bs;
 	}
-	
+
 	private BoardString loadBoard(String path) {
 		try (Scanner s = new Scanner(Thread.currentThread().getContextClassLoader().getResourceAsStream(path))) {
 			int width = 0;
@@ -36,11 +37,32 @@ public class GameLogicService {
 			return new BoardString(buffer.toString(), width);
 		}
 	}
-	
+
 	public void movement(Game game) {
-		
-		//game.getPlayer()
-		
+
+
+		for(int i =0; i<game.getPlayer().size();i++) {
+			Coordinates c = game.getPlayer().get(i).getCoordinates();
+
+			//while (c.getX()!=9 && c.getY()!=18) {
+				int x = game.getPlayer().get(i).getCoordinates().getX();
+				int y = game.getPlayer().get(i).getCoordinates().getY();
+
+				Coordinates c1 = new Coordinates(x+1,y);
+				Coordinates c2 = new Coordinates(x+1,y-1);
+				Coordinates c3 = new Coordinates(x-1,y+1);
+				Coordinates c4 = new Coordinates(x-1,y-1);
+				Coordinates c5 = new Coordinates(x,y+1);
+				Coordinates c6 = new Coordinates(x+1,y+1);
+				Coordinates c7 = new Coordinates(x-1,y);
+				Coordinates c8 = new Coordinates(x,y-1);
+
+				
+				
+			
+
+		}
+
 	}
 
 }
