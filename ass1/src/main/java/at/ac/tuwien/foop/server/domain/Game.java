83cacb8a18c11579ec34082aca279b7f48e7c695
David Seebacher <dseebacher@gmail.com>
2015-05-16 23:25:58 +0200

    test

--- ass1/src/main/java/at/ac/tuwien/foop/server/domain/Game.java
+++ ass1/src/main/java/at/ac/tuwien/foop/server/domain/Game.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import at.ac.tuwien.foop.domain.Board;
+import at.ac.tuwien.foop.domain.Board.Field;
+import at.ac.tuwien.foop.domain.Coordinates;
 import at.ac.tuwien.foop.domain.Player;
 import at.ac.tuwien.foop.server.event.GameEvent;
 import at.ac.tuwien.foop.server.event.GameEvent.Type;
@@ -55,24 +57,34 @@ public class Game {
 		}
 		// calculate next step
 		fireGameEvent(new GameEvent(Type.UPDATE));
-		
-		//call movementmethod here
+
+		// call movementmethod here
 		service.movement(this);
 	}
 
 	/**
 	 * A player joins the game if the maximum amount of players is not reached
-	 * yet.
+	 * yet and no other player with the same name joined before.
 	 * 
 	 * @return true if the player joined, false otherwise
 	 */
-	public synchronized boolean join(Player newPlayer) {
-		if (player.size() < MAX_PLAYERS) {
-			player.add(newPlayer);
+	public synchronized boolean join(String name) {
+		if (player.size() < MAX_PLAYERS && !player.stream().anyMatch(p -> p.name().equals(name))) {
+			
+			player.add(new Player(name, findFreeStartingCoordinates()));
 			fireGameEvent(new GameEvent(Type.NEW_PLAYER));
 			return true;
 		}
 		return false;
+	}
+
+	private Coordinates findFreeStartingCoordinates() {
+		Field[][] f = board.fields();
+		for (int i = 0; i < f.length; i++) {
+			for (int j = 0; j < f[i].length; j++) {
+//				if (f[i][j] == Field.)
+			}
+		}
 	}
 
 	public void leave(Player p) {
@@ -82,22 +94,32 @@ public class Game {
 		}
 		fireGameEvent(new GameEvent(Type.REMOVE_PLAYER));
 	}
-	
+
 	public BoardString boardString() {
 		return boardString;
 	}
-	
+
 	public Board board() {
 		return board;
 	}
-	
+
 	public void setBoard(BoardString bs) {
 		boardString = bs;
 		board = Board.createBoard(bs.board, bs.width);
 	}
-	
+
 	public List<Player> getPlayer() {
 		return player;
 	}
-	
+
+	public void movePlayer(String name, Coordinates coordinates) {
+		player.stream().filter(e -> e.name().equals(name)).findFirst()
+				.orElseThrow(IllegalArgumentException::new)
+				.moveTo(coordinates.x, coordinates.y);
+	}
+
+	public Player getPlayer(String name) {
+		return player.stream().filter(e -> e.name().equals(name)).findFirst()
+				.orElseThrow(IllegalArgumentException::new);
+	}
 }
