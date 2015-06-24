f883b67831b86f891093fb927d932c2005a0904f
David Seebacher <dseebacher@gmail.com>
2015-06-24 12:44:48 +0200

    Add the random name generator :D

--- ass1/src/main/java/at/ac/tuwien/foop/client/RandomNameGenerator.java
+++ ass1/src/main/java/at/ac/tuwien/foop/client/RandomNameGenerator.java
@@ -1,7 +1,19 @@
 package at.ac.tuwien.foop.client;
 
+import java.util.Random;
+
 public class RandomNameGenerator {
+
+	final static private String[] names = { "Charles Babbage", "George Boole",
+			"Vannevar Bush", "Alan Turing", "Edgar Codd", "John Backus",
+			"Dennis Ritchie", "James Gosling", "Bjarne Stroustrup",
+			"Guido vanRossum", "Yukihiro Matsumoto", "Gavin King",
+			"Ken Thompson", "Linus Torvalds", "Richard Stallman",
+			"Tim Berners-Lee", "Ada Lovelace", "Heinz Zemanek",
+			"Gottfried-Wilhelm Leibnitz", "Joseph-Marie Jacquard",
+			"Alonzo Church", "Konrad Zuse", "Grace Hopper", "John vonNeumann" };
+	
 	public static String name() {
-		return "foo";
+		return names[new Random().nextInt(names.length - 1)].split(" ")[0] + " " + names[new Random().nextInt(names.length - 1)].split(" ")[1];
 	}
 }
